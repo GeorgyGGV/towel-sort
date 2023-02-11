6974362856acaac2b6406149fee9e4407fb5e539
GeorgyGGVe <patriotge@mail.ru>
2023-02-12 01:54:22 +0300

    feat: sort-func finished

--- src/index.js
+++ src/index.js
@@ -2,5 +2,17 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+
+  if((matrix != '') && (matrix != undefined)){
+    for(let i = 0; i<matrix.length; i++){
+        if(i%2==0){
+          matrix[i] = matrix[i].join();
+        } else { 
+            matrix[i] = matrix[i].reverse().join();
+        }
+    }
+    return matrix.join().split(',');
+    //console.log(matrix.join().split(','));
+} else {console.log('hi');  return [];}
+
 }
